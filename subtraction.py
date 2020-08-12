f6c8943fb54ab90f6354849039c7cc6fdb0131e8
Harshitha1808 <65725738+Harshitha1808@users.noreply.github.com>
2020-08-12 21:35:21 +0530

    adding subtraction file

---
+++ subtraction.py
@@ -0,0 +1,9 @@
+python program to subtract 
+
+def subtraction(x,y):  
+    sub=x-y
+    return sub
+num1=int(input("please enter first number: "))
+num2=int(input("please enter second number: "))
+
+print("Subtraction is: ",subtraction(num1,num2))
