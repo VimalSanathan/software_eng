dcc3bdd342e5e6a2a1199bff9c4effdef34bbc0b
Monika-23 <65751212+Monika-23@users.noreply.github.com>
2020-09-02 12:13:28 +0530

    main file

---
+++ main.py
@@ -0,0 +1,10 @@
+from addition import *
+from subtraction import *
+from division import *
+from multiply import *
+a = int(input("Enter a number: "))
+b= int(input("Enter a number: "))
+addition(a,b)
+subtraction(a,b)
+division(a,b)
+multiply(a,b)
